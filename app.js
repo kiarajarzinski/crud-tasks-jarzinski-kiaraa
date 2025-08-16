ecf76b887d035c9e428c16edc93aae449a9602a2
Kiara <jarzinskikiara@gmail.com>
2025-08-16 20:29:38 -0300

    se realizo la estructura de app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import taskRoutes from "./src/routes/tasks.routes.js"
+import userRoutes from "./src/routes/users.routes.js";
+import sequelize from "./src/config/database.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/tasks", taskRoutes);
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+sequelize.sync().then(() => {
+    console.log("Base de datos conectada correctamente");
+    app.listen(PORT, () => {
+        console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
+    });
+});
